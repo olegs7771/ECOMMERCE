daa0d1147258cf0915b9612801ffccf0e3a041fc
olegs7771 <olegs7771@gmail.com>
2020-12-24 22:08:48 +0200

    ok

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,17 +4,25 @@ const Category = require('../models/Category');
 const AppErrorHandler = require('../utils/AppError');
 const slugify = require('slugify');
 
+// CREATE GATEGORY
 const create = asyncCatch(async (req, res, next) => {
-  // 1) CREATE GATEGORY
   const body = { name: req.body.name, slug: slugify(req.body.name) };
   const category = await Category.create(body);
   res.status(200).json({ status: 'success', data: category });
 });
+////////////////////////////////////////////////////////////
 
+// FIND ALL CATEGORIES
+const list = asyncCatch(async (req, res, next) => {
+  console.log('list');
+  const categories = await Category.find();
+  res
+    .status(200)
+    .json({ status: 'success', qnt: categories.length, data: categories });
+});
 /////////////////////////////////////////////////////
 const read = asyncCatch(async (req, res, next) => {});
 const update = asyncCatch(async (req, res, next) => {});
 const remove = asyncCatch(async (req, res, next) => {});
-const list = asyncCatch(async (req, res, next) => {});
 
 module.exports = { create, read, update, remove, list };
