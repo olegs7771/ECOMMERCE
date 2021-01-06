a4af00e47a6eeaf7100fd8bfae6f0f476c709edb
olegs7771 <olegs7771@gmail.com>
2021-01-06 12:23:47 +0200

    ok

--- client/src/components/admin/product/ProductForm.js
+++ client/src/components/admin/product/ProductForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-export default function CategoryForm() {
+export default function CategoryForm({ open }) {
   const _setValue = (e) => {};
 
   const initialState = {
@@ -20,131 +20,150 @@ export default function CategoryForm() {
   };
 
   const [values, setValue] = useState(initialState);
+  const [showForm, setShowForm] = useState(false);
 
   // const { title } = values;
 
   const _onChange = (e) => {
     setValue({ ...values, [e.target.name]: e.target.value });
-    console.log(e.target.name + '-----------' + e.target.value);
   };
   const _onSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
-    <form onSubmit={_onSubmit} className="form  category__form product__form">
+    <form
+      onSubmit={_onSubmit}
+      className={
+        open
+          ? 'form  category__form product__form'
+          : 'form  category__form product__form  product__form--close'
+      }
+    >
       <div className="heading-3">Add Product</div>
-      <div className="form-group">
-        <label>
-          <div className="form-label--name">Title</div>
-          <input
-            type="text"
-            name="title"
-            className="form-input  category__input"
-            value={values.name}
-            onChange={_onChange}
-            required
-          />
-        </label>
-      </div>
-      <div className="form-group">
-        <label>
-          <div className="form-label--name">Description</div>
-          <textarea
-            name="description"
-            value={values.description}
-            onChange={_onChange}
-            className="form-input  category__input"
-          />
-        </label>
-      </div>
-      <div className="form-group">
-        <label>
-          <div className="form-label--name">Price</div>
-          <input
-            type="text"
-            name="price"
-            className="form-input  category__input"
-            value={values.price}
-            onChange={_onChange}
-            required
-          />
-        </label>
-      </div>
-      <div className="form-group">
-        <label>
-          <div className="form-label--name">Quantity</div>
-          <input
-            type="number"
-            name="quantity"
-            className="form-input  category__input"
-            value={values.quantity}
-            onChange={_onChange}
-            required
-          />
-        </label>
-      </div>
-      <div className="form-label--name">Shipping</div>
-      <div className="form-group form-group__radio">
-        <label className=" form-group__radio-label">
-          <input
-            type="radio"
-            value="Yes"
-            name="shipping"
-            checked={values.shipping === 'Yes'}
-            onChange={_onChange}
-          />
-          <span className="form-label--name">Yes</span>
-        </label>
-        <label className=" form-group__radio-label">
-          <input
-            type="radio"
-            value="No"
-            name="shipping"
-            checked={values.shipping === 'No'}
-            onChange={_onChange}
-          />
+      <div className="product__form-container">
+        {/* LEFT BLOCK  */}
+        <div className="product__form-l-block">
+          <div className="form-group">
+            <label>
+              <div className="form-label--name">Title</div>
+              <input
+                type="text"
+                name="title"
+                className="form-input  category__input"
+                value={values.name}
+                onChange={_onChange}
+                required
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              <div className="form-label--name">Description</div>
+              <textarea
+                name="description"
+                value={values.description}
+                onChange={_onChange}
+                className="form-input  category__input--textarea"
+                rows={8}
+              />
+            </label>
+          </div>
+        </div>
+        {/* CENTER BLOCK  */}
+        <div className="product__form-c-block">
+          <div className="form-group">
+            <label>
+              <div className="form-label--name">Quantity</div>
+              <input
+                type="number"
+                name="quantity"
+                className="form-input  category__input"
+                value={values.quantity}
+                onChange={_onChange}
+                required
+              />
+            </label>
+          </div>
 
-          <span className="form-label--name">No</span>
-        </label>
-      </div>
-      <div className="form-group">
-        <label>
-          <div className="form-label--name">Pick your Color</div>
-          <select
-            name="color"
-            value={values.color}
-            onChange={_onChange}
-            className="form-input  category__input"
-          >
-            <option>Select Color</option>
-            {initialState.colors.map((color) => (
-              <option value={color} key={color}>
-                {color}
-              </option>
-            ))}
-          </select>
-        </label>
-      </div>
-      <div className="form-group">
-        <label>
-          <div className="form-label--name">Pick your Brand</div>
-          <select
-            name="brand"
-            value={values.brand}
-            onChange={_onChange}
-            className="form-input  category__input"
-          >
-            <option>Select Brand</option>
-            {initialState.brands.map((brand) => (
-              <option value={brand} key={brand}>
-                {brand}
-              </option>
-            ))}
-          </select>
-        </label>
-      </div>
+          <div className="form-group">
+            <label>
+              <div className="form-label--name">Pick your Color</div>
+              <select
+                name="color"
+                value={values.color}
+                onChange={_onChange}
+                className="form-input  category__input"
+              >
+                <option>Select Color</option>
+                {initialState.colors.map((color) => (
+                  <option value={color} key={color}>
+                    {color}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              <div className="form-label--name">Pick your Brand</div>
+              <select
+                name="brand"
+                value={values.brand}
+                onChange={_onChange}
+                className="form-input  category__input"
+              >
+                <option>Select Brand</option>
+                {initialState.brands.map((brand) => (
+                  <option value={brand} key={brand}>
+                    {brand}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+        </div>
+        {/* RIGHT BLOCK  */}
+        <div className="product__form-r-block">
+          <div className="form-group">
+            <label>
+              <div className="form-label--name">Price</div>
+              <input
+                type="text"
+                name="price"
+                className="form-input  category__input"
+                value={values.price}
+                onChange={_onChange}
+                required
+              />
+            </label>
+          </div>
+          <div className="form-label--name">Shipping</div>
+          <div className="form-group form-group__radio">
+            <label className=" form-group__radio-label">
+              <input
+                type="radio"
+                value="Yes"
+                name="shipping"
+                checked={values.shipping === 'Yes'}
+                onChange={_onChange}
+              />
+              <span className="form-label--name">Yes</span>
+            </label>
+            <label className=" form-group__radio-label">
+              <input
+                type="radio"
+                value="No"
+                name="shipping"
+                checked={values.shipping === 'No'}
+                onChange={_onChange}
+              />
 
+              <span className="form-label--name">No</span>
+            </label>
+          </div>
+        </div>
+      </div>
       <input type="submit" value="save" className="btn btn-auth" />
     </form>
   );
