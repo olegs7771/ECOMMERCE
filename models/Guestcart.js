6ea39625ebe23d6ce94ea8c462d3d008d0687a16
olegs7771 <olegs7771@gmail.com>
2021-02-06 18:09:38 +0200

    ok

--- models/Guestcart.js
+++ models/Guestcart.js
@@ -1,8 +1,29 @@
 const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+const guestcartSchema = new mongoose.Schema(
+  {
+    guestId: {
+      type: String,
+      required: [true, 'Provide guest userId'],
+    },
 
-const guestSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: [true, 'Provide guest userId'],
+    userId: {
+      type: ObjectId,
+      ref: 'User',
+    },
+    cart: [
+      {
+        productId: {
+          type: String,
+          required: [true, 'Provide product id'],
+        },
+      },
+    ],
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const Guestcart = mongoose.model('Guestcart', guestcartSchema);
+module.exports = Guestcart;
