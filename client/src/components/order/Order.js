ae71d598c66cad4c2f9c2b1b31e1702712a0c27a
olegs7771 <olegs7771@gmail.com>
2021-02-22 12:59:35 +0200

    ok

---
+++ client/src/components/order/Order.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Order() {
+  return (
+    <div>
+      <h1>Contact us</h1>
+      {/* <address>
+         52nd Street<br>
+         New York,<br>
+         NY 10019<br>
+         USA
+      </address> */}
+    </div>
+  );
+}
