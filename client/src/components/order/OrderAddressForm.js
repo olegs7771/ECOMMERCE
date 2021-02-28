60504b3f07d4a4a1ed4b75a9a32da820761548c9
olegs7771 <olegs7771@gmail.com>
2021-02-28 16:29:01 +0200

    ok

--- client/src/components/order/OrderAddressForm.js
+++ client/src/components/order/OrderAddressForm.js
@@ -8,11 +8,12 @@ import { createOrderGuestAction } from '../../store/actions/orderAction';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
 
-export default function OrderAddressForm() {
+export default function OrderAddressForm({ cartId }) {
   const dispatch = useDispatch();
   // REDUX
   const authRedux = useSelector((state) => state.auth);
   const cookieRedux = useSelector((state) => state.cookie.cookie);
+  const errorsRedux = useSelector((state) => state.error.errors);
 
   // STATE
   const initialState = {
@@ -47,20 +48,22 @@ export default function OrderAddressForm() {
   const [showCity, setShowCity] = useState(false); //toggle open input field for other city
 
   const _onChange = (e) => {
-    console.log('e', e.target.name);
-    if (e.target === undefined) {
-      setValues({ ...values, phone: e }); //for phone field
-    } else {
-      // setShowCity(false);
-      setValues({ ...values, [e.target.name]: e.target.value });
-    }
-    //Handle other city not in list
-    if (e.target.name === 'city' && e.target.value === 'other') {
-      console.log('other city', e.target.value);
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  const _onChangePhone = (e) => {
+    setValues({ ...values, phone: e });
+  };
+  const _onChangeProvince = (e) => {
+    setShowCity(false);
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  const _onChangeCity = (e) => {
+    console.log('e.target.value city', e.target.value);
+    if (e.target.value === 'other') {
       setShowCity(true);
-    }
-    if (e.target.name === 'province_territory_value') {
-      setShowCity(false);
+      setValues({ ...values, [e.target.name]: e.target.value });
+    } else {
+      setValues({ ...values, [e.target.name]: e.target.value });
     }
   };
 
@@ -70,11 +73,13 @@ export default function OrderAddressForm() {
       fname: values.first_name,
       lname: values.last_name,
       email: values.email,
+      province: values.province_territory_value,
       city: values.city,
       suit: values.suit_apt,
       street: values.street_address,
       phone: values.phone,
       zipcode: values.zipcode,
+      cartId,
     };
     let data;
     if (authRedux.isAuthenticated) {
@@ -102,9 +107,12 @@ export default function OrderAddressForm() {
           label="First Name"
           name="first_name"
           placeholder="John"
-          styles={{ title: 'form-label--name' }}
+          styles={{
+            title: 'form-label--name',
+            form_group: 'form-group order__buyer__address__form-group',
+          }}
           required={true}
-          //  error,
+          error={errorsRedux.fname}
           //  _checkField,
         />
         <div className="order__buyer__address__row__gap"></div>
@@ -114,8 +122,12 @@ export default function OrderAddressForm() {
           label="Last Name"
           name="last_name"
           placeholder="Brown"
-          styles={{ title: 'form-label--name' }}
+          styles={{
+            title: 'form-label--name',
+            form_group: 'form-group order__buyer__address__form-group',
+          }}
           required={true}
+          error={errorsRedux.lname}
         />
       </div>
       <div className="order__buyer__address__row">
@@ -125,7 +137,11 @@ export default function OrderAddressForm() {
           label="Business Name"
           name="business_name"
           placeholder="Your business name.. (optional)"
-          styles={{ title: 'form-label--name' }}
+          styles={{
+            title: 'form-label--name',
+            form_group: 'form-group order__buyer__address__form-group',
+          }}
+          error={errorsRedux.business_name}
         />
         <div className="order__buyer__address__row__gap"></div>
 
@@ -135,26 +151,32 @@ export default function OrderAddressForm() {
           label="Email"
           name="email"
           placeholder=" example@somemail.com "
-          styles={{ title: 'form-label--name' }}
+          styles={{
+            title: 'form-label--name',
+            form_group: 'form-group order__buyer__address__form-group',
+          }}
           containerClass={{ width: '100%' }}
           required={true}
+          error={errorsRedux.email}
         />
       </div>
       <div className="order__buyer__address__row">
         <div className="order__buyer__address__row__wrapper">
           <SelectInputForm
-            _onChange={_onChange}
+            _onChange={_onChangeProvince}
             array={initialState.province_territory}
             value={values.province_territory_value}
             styles={{
               title: 'form-label--name form-label--name__select',
-              form_input: 'form-input-select',
+              form_input: 'form-input-select ',
+              form_group: 'form-group order__buyer__address__form-group-select',
             }}
             name="province_territory_value"
             label="Province/Territory"
             select="Select Province/Territory"
             title="Select Province/Territory"
             required={true}
+            error={errorsRedux.province}
           />
         </div>
         <div className="order__buyer__address__row__gap"></div>
@@ -165,24 +187,31 @@ export default function OrderAddressForm() {
             label="City/Town"
             name="city"
             placeholder=" other city/town "
-            styles={{ title: 'form-label--name' }}
+            styles={{
+              title: 'form-label--name',
+              form_group: 'form-group order__buyer__address__form-group',
+            }}
             required={true}
+            error={errorsRedux.city}
           />
         ) : (
           <div className="order__buyer__address__row__wrapper">
             <CanadianCitiesFieldForm
-              _onChange={_onChange}
+              _onChange={_onChangeCity}
               province={values.province_territory_value}
               value={values.city}
               styles={{
                 title: 'form-label--name form-label--name__select',
                 form_input: 'form-input-select',
+                form_group:
+                  'form-group order__buyer__address__form-group-select',
               }}
               name="city"
               label="City/Town"
               select="Select City/Town"
               title="City/Town"
               required={true}
+              error={errorsRedux.city}
             />
           </div>
         )}
@@ -195,8 +224,12 @@ export default function OrderAddressForm() {
           label="Suit/Apt"
           name="suit_apt"
           placeholder=" Suit or apartment "
-          styles={{ title: 'form-label--name' }}
+          styles={{
+            title: 'form-label--name',
+            form_group: 'form-group order__buyer__address__form-group',
+          }}
           required={true}
+          error={errorsRedux.suit}
         />
         <div className="order__buyer__address__row__gap"></div>
 
@@ -206,28 +239,40 @@ export default function OrderAddressForm() {
           label="Street"
           name="street_address"
           placeholder=" Your street name.."
-          styles={{ title: 'form-label--name' }}
+          styles={{
+            title: 'form-label--name',
+            form_group: 'form-group order__buyer__address__form-group',
+          }}
           required={true}
+          error={errorsRedux.street}
         />
       </div>
       <div className="order__buyer__address__row">
-        <div className="order__buyer__address__row__wrapper mb-sm">
+        <div className="order__buyer__address__row__wrapper ">
           <div className="order__buyer__address__row__phone--lable">
             Phone
             <span className="order__buyer__address__row__phone--lable-icon">
               *
             </span>
           </div>
-          <div className="order__buyer__address__row__wrapper">
+          <div className="order__buyer__address__row__wrapper--phone">
             <PhoneInput
               value={values.phone}
-              onChange={(phone) => _onChange(phone)}
+              onChange={(phone) => _onChangePhone(phone)}
               onlyCountries={['ca', 'us']}
               enableAreaCodes={['ca', 'usa']}
               country={'ca'}
               inputClass="order__buyer__address__row__phone"
               required={true}
+              isValid={!errorsRedux.phone}
             />
+            {errorsRedux.phone ? (
+              <div className="form-input__error">
+                <div className="form-input__error--text order__buyer__address__row__phone--error ">
+                  {errorsRedux.phone}
+                </div>
+              </div>
+            ) : null}
           </div>
         </div>
         <div className="order__buyer__address__row__gap"></div>
@@ -237,9 +282,13 @@ export default function OrderAddressForm() {
           label="Zipcode"
           name="zipcode"
           placeholder=" Zipcode "
-          styles={{ title: 'form-label--name' }}
+          styles={{
+            title: 'form-label--name',
+            form_group: 'form-group order__buyer__address__form-group',
+          }}
           containerClass={{ width: '100%' }}
           required={true}
+          error={errorsRedux.zipcode}
         />
       </div>
 
