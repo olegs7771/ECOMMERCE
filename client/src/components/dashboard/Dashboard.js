ea333745da90a262e37d03279dc66a9404a6f40a
olegs7771 <olegs7771@gmail.com>
2021-03-06 21:18:58 +0200

    ok

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { drawerToggle } from '../../store/actions/drawerAction';
 
@@ -7,13 +7,16 @@ export default function Dashboard(props) {
   const dispatch = useDispatch();
   const drawerRedux = useSelector((state) => state.drawer.drawer);
 
+  // STATE
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
     <div className="page dashboard">
       <div className="container">
         <div className="dashboard__wrapper">
           <div className="dashboard__nav">
             <ul className="dashboard__nav__list">
-              <li className="dashboard__nav__item">
+              <li className="dashboard__nav__item dashboard__nav__item__orders">
                 <a href="#!" className="dashboard__nav__link">
                   Orders
                 </a>
