7a65be68f733d26853d740018b1f0e84dcbbe85a
olegs7771 <olegs7771@gmail.com>
2021-03-19 21:16:08 +0200

    ok

--- controllers/guestController.js
+++ controllers/guestController.js
@@ -15,58 +15,59 @@ const findJwtToken = (cookies) => {
 
 // ON LOADING APP GET sessionId,userId,token TO COOKIES
 const getGuestCookieToken = asyncCatch(async (req, res, next) => {
-  const sessionId = crypto.randomBytes(10).toString('hex');
-  const guestId = uuidv4();
-  const payload = {
-    sub: guestId,
-    iss: `${req.protocol}://${req.get('host')}`,
-  };
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_COOKIE_EXP_GUEST, //70d
-  });
+  console.log('get guest cookies');
+  // const sessionId = crypto.randomBytes(10).toString('hex');
+  // const guestId = uuidv4();
+  // const payload = {
+  //   sub: guestId,
+  //   iss: `${req.protocol}://${req.get('host')}`,
+  // };
+  // const token = jwt.sign(payload, process.env.JWT_SECRET, {
+  //   expiresIn: process.env.JWT_COOKIE_EXP_GUEST, //70d
+  // });
 
-  //Send token to cookies
-  res.cookie('guest', token, {
-    expires: new Date(
-      Date.now() +
-        parseInt(process.env.GUEST_COOKIE_EXP, 10) * 24 * 60 * 60 * 1000
-    ),
-    // maxAge: parseInt(process.env.GUEST_COOKIE_EXP, 10), ///60s
-    httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-    sameSite: true,
-  });
-  res.cookie('sessionId', sessionId, {
-    expires: new Date(
-      Date.now() +
-        parseInt(process.env.GUEST_COOKIE_EXP, 10) * 24 * 60 * 60 * 1000
-    ),
-    // maxAge: parseInt(process.env.GUEST_COOKIE_EXP, 10), ///60s
-    // httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-    sameSite: true,
-  });
-  res.cookie('cart_exp', guestId, {
-    expires: new Date(
-      Date.now() +
-        parseInt(process.env.CART_COOKIE_EXP, 10) * 24 * 60 * 60 * 1000 //30d
-    ),
-    // maxAge: parseInt(process.env.GUEST_COOKIE_EXP, 10), ///60s
-    // httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-    sameSite: true,
-  });
-  res.cookie('guestId', guestId, {
-    expires: new Date(
-      Date.now() +
-        parseInt(process.env.GUEST_COOKIE_EXP, 10) * 24 * 60 * 60 * 1000
-    ),
-    // maxAge: parseInt(process.env.JWT_COOKIE_EXP, 10), ///60s
-    // httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-    sameSite: true,
-  });
-  res.status(200).json({ status: 'success', token, data: sessionId, guestId });
+  // //Send token to cookies
+  // res.cookie('guest', token, {
+  //   expires: new Date(
+  //     Date.now() +
+  //       parseInt(process.env.GUEST_COOKIE_EXP, 10) * 24 * 60 * 60 * 1000
+  //   ),
+  //   // maxAge: parseInt(process.env.GUEST_COOKIE_EXP, 10), ///60s
+  //   httpOnly: true,
+  //   secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  //   sameSite: true,
+  // });
+  // res.cookie('sessionId', sessionId, {
+  //   expires: new Date(
+  //     Date.now() +
+  //       parseInt(process.env.GUEST_COOKIE_EXP, 10) * 24 * 60 * 60 * 1000
+  //   ),
+  //   // maxAge: parseInt(process.env.GUEST_COOKIE_EXP, 10), ///60s
+  //   // httpOnly: true,
+  //   secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  //   sameSite: true,
+  // });
+  // res.cookie('cart_exp', guestId, {
+  //   expires: new Date(
+  //     Date.now() +
+  //       parseInt(process.env.CART_COOKIE_EXP, 10) * 24 * 60 * 60 * 1000 //30d
+  //   ),
+  //   // maxAge: parseInt(process.env.GUEST_COOKIE_EXP, 10), ///60s
+  //   // httpOnly: true,
+  //   secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  //   sameSite: true,
+  // });
+  // res.cookie('guestId', guestId, {
+  //   expires: new Date(
+  //     Date.now() +
+  //       parseInt(process.env.GUEST_COOKIE_EXP, 10) * 24 * 60 * 60 * 1000
+  //   ),
+  //   // maxAge: parseInt(process.env.JWT_COOKIE_EXP, 10), ///60s
+  //   // httpOnly: true,
+  //   secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  //   sameSite: true,
+  // });
+  // res.status(200).json({ status: 'success', token, data: sessionId, guestId });
 });
 
 // CHECK IF GUEST HAS VALID TOKEN
