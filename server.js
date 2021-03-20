0b0e5f97e64df7bafe7002abf09790b1ec0dce57
olegs7771 <olegs7771@gmail.com>
2021-03-20 07:10:55 +0200

    ok

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => console.log(`connected to ${DB}`))
-  .catch((err) => console.log('error to connect to DB', err));
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//   })
+//   .then(() => console.log(`connected to ${DB}`))
+//   .catch((err) => console.log('error to connect to DB', err));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
